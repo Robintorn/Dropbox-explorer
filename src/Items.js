0268fbdafaf6bfb2181a0690c83f772d7a5b2564
robin torn <robin.torn.98@gmail.com>
2018-06-03 16:30:56 +0200

    kan ladda upp visa filer, men fett jävla luddig kod

--- src/Items.js
+++ src/Items.js
@@ -39,7 +39,7 @@ class Items extends Component {
     }
   
     componentWillMount() {
-      let dbx = new Dropbox({ accessToken: "BW7-qRPIdfAAAAAAAAAAqOmgydS8vuJdIJja8Wz3Xx00_gpmTnRsSQBMfkrKVXA_" });
+      let dbx = new Dropbox({ accessToken: "BW7-qRPIdfAAAAAAAAAAxXtENJaBF6PObfF7GcSOFtdYMseuO_XndDdDhrCOpyWc" });
       dbx.filesListFolder({path: ''})
       .then((response) => this.onClick(response.entries))
       .catch((error) => {
@@ -53,10 +53,75 @@ class Items extends Component {
         items: response
       })
     }
+
+    uploadFile = () => {
+      
+    const UPLOAD_FILE_SIZE_LIMIT = 150 * 1024 * 1024;
+    var dbx = new Dropbox({ accessToken: "BW7-qRPIdfAAAAAAAAAAxXtENJaBF6PObfF7GcSOFtdYMseuO_XndDdDhrCOpyWc" });
+    var fileInput = document.getElementById('upload-file');
+    var file = fileInput.files[0];
+    
+    
+    if (file.size < UPLOAD_FILE_SIZE_LIMIT) { // File is smaller than 150 Mb - use filesUpload API
+      dbx.filesUpload({path: '/' + file.name, contents: file})
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+    }
+    
+    else {
+        const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
+        var workItems = [];     
+      
+        var offset = 0;
+        while (offset < file.size) {
+          var chunkSize = Math.min(maxBlob, file.size - offset);
+          workItems.push(file.slice(offset, offset + chunkSize));
+          offset += chunkSize;
+        } 
+          
+        const task = workItems.reduce((acc, blob, idx, items) => {
+          if (idx === 0) {
+            // Starting multipart upload of file
+            return acc.then(function() {
+              return dbx.filesUploadSessionStart({ close: false, contents: blob})
+                        .then(response => response.session_id)
+            });          
+          } else if (idx < items.length-1) {  
+            // Append part to the upload session
+            return acc.then(function(sessionId) {
+             var cursor = { session_id: sessionId, offset: idx * maxBlob };
+             return dbx.filesUploadSessionAppendV2({ cursor: cursor, close: false, contents: blob }).then(() => sessionId); 
+            });
+          } else {
+            // Last chunk of data, close session
+            return acc.then(function(sessionId) {
+              var cursor = { session_id: sessionId, offset: file.size - blob.size };
+              var commit = { path: '/' + file.name, mode: 'add', autorename: true, mute: false };              
+              return dbx.filesUploadSessionFinish({ cursor: cursor, commit: commit, contents: blob });           
+            });
+          }          
+        }, Promise.resolve());
+      task.then(function(result) {
+        console.log(result);
+      }).catch(function(error) {
+        console.error(error);
+      });
+      
+    }
+    return false;
+  }
   
     render(){
     return(
       <div>
+        <form onSubmit={this.uploadFile}>
+            <input type="file" id="upload-file" placeholder="Upload file" ref="input" />
+            <button type="submit">Upload</button>
+        </form>
         {this.state.items.map((item) => {
           return(
             <RenderItems key={item.id} text={item}/>
