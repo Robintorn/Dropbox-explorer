2f95640c5f14446c4111b7db0b66dd2a5b4a85b7
robin torn <robin.torn.98@gmail.com>
2018-06-06 18:55:25 +0200

    update

--- src/Items.js
+++ src/Items.js
@@ -114,47 +114,6 @@ class Items extends Component {
           console.error(error);
         });
     }
-    
-    else {
-        const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
-        var workItems = [];     
-      
-        var offset = 0;
-        while (offset < file.size) {
-          var chunkSize = Math.min(maxBlob, file.size - offset);
-          workItems.push(file.slice(offset, offset + chunkSize));
-          offset += chunkSize;
-        } 
-          
-        const task = workItems.reduce((acc, blob, idx, items) => {
-          if (idx === 0) {
-            // Starting multipart upload of file
-            return acc.then(() => {
-              return dbx.filesUploadSessionStart({ close: false, contents: blob})
-                        .then(response => response.session_id)
-            });          
-          } else if (idx < items.length-1) {  
-            // Append part to the upload session
-            return acc.then((sessionId) => {
-             var cursor = { session_id: sessionId, offset: idx * maxBlob };
-             return dbx.filesUploadSessionAppendV2({ cursor: cursor, close: false, contents: blob }).then(() => sessionId); 
-            });
-          } else {
-            // Last chunk of data, close session
-            return acc.then((sessionId) => {
-              var cursor = { session_id: sessionId, offset: file.size - blob.size };
-              var commit = { path: '/' + file.name, mode: 'add', autorename: true, mute: false };              
-              return dbx.filesUploadSessionFinish({ cursor: cursor, commit: commit, contents: blob });           
-            });
-          }          
-        }, Promise.resolve());
-      task.then((result) => {
-        console.log(result);
-      }).catch((error) => {
-        console.error(error);
-      });
-      
-    }
     return false;
   }
 
