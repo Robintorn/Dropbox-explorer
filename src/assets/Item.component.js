6e88ca225235db50a7e39b8b1927a7f570b77150
robin torn <robin.torn.98@gmail.com>
2019-03-31 22:29:10 +0200

    update

---
+++ src/assets/Item.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OnDownload from '../assets/OnDownload';
+import LoadThumbnail from '../assets/LoadThumbnail';
+
+const Item = () => {
+    const extensions = /\.(jpg|png|PNG|gif)\b/;
+    return(
+      <div>
+      </div>
+    )
+  };
+
+export default Item;
--- src/assets/OnDownload.js
+++ src/assets/OnDownload.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Dropbox } from 'dropbox';
 
 const onDownload = ({file}) => {
-      let path = file.path_lower;
-      let dbx = new Dropbox({ accessToken: localStorage.getItem("token") });
-      dbx.filesGetTemporaryLink({path}) 
-      .then(data => { 
-        let fileName = file.path_display;
-        document.getElementById(`${fileName}`).setAttribute('href', data.link);
-      })
-      .catch(err => console.log(err));
+  let path = file.path_lower;
+  let dbx = new Dropbox({ accessToken: localStorage.getItem("token") });
+  dbx.filesGetTemporaryLink({path}) 
+  .then(data => { 
+    let fileName = file.path_display;
+    document.getElementById(`${fileName}`).setAttribute('href', data.link);
+  })
+  .catch(err => console.log(err));
       return (
         <a id={file.path_display} href="">Download</a>
       )
